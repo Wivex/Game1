87ee7e1503829f24baf1c70f2f1ad274219b282f
Wivex <wivexorium@gmail.com>
2019-07-01 22:29:58 +0300

    - restored some lost progress - added unity autosaver

--- Assets/3rd-Party/AutoSave/Editor/AutoSaveExtension.cs
+++ Assets/3rd-Party/AutoSave/Editor/AutoSaveExtension.cs
@@ -1,26 +1,25 @@
 ﻿using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-namespace EckTechGames
+[InitializeOnLoad]
+public class AutoSaveExtension
 {
-	[InitializeOnLoad]
-	public class AutoSaveExtension
-	{
-		// Static constructor that gets called when unity fires up.
-		static AutoSaveExtension()
-		{
-			EditorApplication.playmodeStateChanged += AutoSaveWhenPlaymodeStarts;
-		}
+    // Static constructor that gets called when unity fires up.
+    static AutoSaveExtension()
+    {
+        EditorApplication.playModeStateChanged += AutoSaveWhenPlaymodeStarts;
+    }
 
-		private static void AutoSaveWhenPlaymodeStarts()
-		{
-			// If we're about to run the scene...
-			if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
-			{
-				// Save the scene and the assets.
-				EditorApplication.SaveScene();
-				AssetDatabase.SaveAssets();
-			}
-		}
-	}
+    static void AutoSaveWhenPlaymodeStarts(PlayModeStateChange state)
+    {
+        // If we're about to run the scene...
+        if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+        {
+            // Save the scene and the assets.
+            EditorSceneManager.SaveScene(SceneManager.GetActiveScene());
+            AssetDatabase.SaveAssets();
+        }
+    }
 }
