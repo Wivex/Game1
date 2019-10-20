c9a7acb2fb6ad153aac23c86671f94e3527a344a
Wivex <wivexorium@gmail.com>
2019-10-20 15:35:42 +0300

    - changed root Canvas type to World view ot be abled to control and adjust Camera - implemented Universal game view scaler to fit any device resolution

---
+++ Assets/Engine/Managers/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int targetFramerate;
+
+    #region MANAGER INITIALIZATION
+
+    /// <summary>
+    /// Can't access static variables and methods from inspector. So we use static instance to do that.
+    /// </summary>
+    public static GameManager i;
+
+    //default initialization of Singleton instance
+    void Awake()
+    {
+        //Check if instance already exists
+        if (i == null)
+            //if not, set instance to this
+            i = this;
+        //If instance already exists and it's not this:
+        else if (i != this)
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of it.
+
+            //Sets this to not be destroyed when reloading scene
+            DontDestroyOnLoad(gameObject);
+    }
+
+
+    #endregion
+
+    void Start()
+    {
+        // NOTE: test if working
+        Application.targetFrameRate = targetFramerate;
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
